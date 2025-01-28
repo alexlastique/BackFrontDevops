b7a99e06801574b30f0d8c45cc066be36e166cfc
Mitake-ktm <kev800da@gmail.com>
2025-01-28 13:53:32 +0100

    Clean API in folder

--- API/main.py
+++ API/main.py
@@ -28,7 +28,7 @@ connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
 def create_db_and_tables():
-    database = Path("../database.db")
+    database = Path("../API/database.db")
     if not database.is_file():
         SQLModel.metadata.create_all(engine)
 
