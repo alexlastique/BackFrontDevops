3751ec1c4116bd8e98c5111820dc15d0e5eb0995
Lomudru <kmejasson@gmail.com>
2025-01-28 16:54:52 +0100

    Add Cors fast api

--- API/main.py
+++ API/main.py
@@ -17,7 +17,21 @@ from send_money import SendMoney
 from cancel import Cancel
 from account import *
 from beneficiary_add import Beneficiary_add
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 logging.basicConfig(level=logging.INFO)
 
