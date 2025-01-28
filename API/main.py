d50b8264ed51897c6b718f32701562301b5c97c8
Alexis Godet <66777879+alexlastique@users.noreply.github.com>
2025-01-28 14:16:52 +0100

    Correction of db

--- API/main.py
+++ API/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 logging.basicConfig(level=logging.INFO)
 
-sqlite_file_name = "database.db"
+sqlite_file_name = "API/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
